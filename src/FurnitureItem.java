8196def6f317ee9300e05d9ea25b1b851e019aa1
Ajay Chavan <ajayc9838@gmail.com>
2022-08-11 15:22:21 +0530

    updated with scanner

--- src/FurnitureItem.java
+++ src/FurnitureItem.java
@@ -11,17 +11,23 @@ public class FurnitureItem {
 
     FurnitureItem() {
         Scanner sc = new Scanner(System.in);
-        furnitureCode = 0;
-        furnitureType = "plastic";
-        gradeOfFurniture = "grade";
-        color = "red";
-        furnitureUsage = "outdoor";
-        System.out.println("Enter price");
+        System.out.println("Enter furniture code");
+        furnitureCode = sc.nextInt();
+        System.out.println("Enter furniture type :");
+        sc.nextLine();
+        furnitureType = sc.nextLine();
+        System.out.println("Enter furniture grade 1, 2, 3 :");
+        gradeOfFurniture = sc.nextLine();
+        System.out.println("Enter furniture color (red, green, blue) :");
+        color = sc.nextLine();
+        System.out.println("Enter furniture type (outdoor/indoor) :");
+        furnitureUsage = sc.nextLine();
+        System.out.println("Enter price :");
         price = sc.nextDouble();
     }
 
     public double calculateDiscount(double discountPercentage) {
-        if (furnitureUsage.equals("outdoor")) {
+        if (furnitureUsage.equalsIgnoreCase("outdoor")) {
             double discountedPrice = price - (price * discountPercentage / 100);
             return discountedPrice;
         } else {
--- src/FurnitureItemMain.java
+++ src/FurnitureItemMain.java
@@ -3,13 +3,12 @@ public class FurnitureItemMain {
     public static void main(String[] args) {
 
         FurnitureItem fItem = new FurnitureItem();
-        System.out.println("Enter price");
-        System.out.println(fItem.furnitureCode);
-        System.out.println(fItem.furnitureType);
-        System.out.println(fItem.gradeOfFurniture);
-        System.out.println(fItem.color);
-        System.out.println(fItem.furnitureUsage);
-        System.out.println(fItem.price);
-        System.out.println(fItem.calculateDiscount(5));
+        System.out.println("Furniture code :" + fItem.furnitureCode);
+        System.out.println("Furniture type :" + fItem.furnitureType);
+        System.out.println("Furniture grade :" + fItem.gradeOfFurniture);
+        System.out.println("Furniture color :" + fItem.color);
+        System.out.println("Furniture Usage :" + fItem.furnitureUsage);
+        System.out.println("Furniture price :" + fItem.price);
+        System.out.println("Dicounted price :" + fItem.calculateDiscount(5));
     }
 }
